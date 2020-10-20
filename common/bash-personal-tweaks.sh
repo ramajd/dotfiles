798665511348e3326fab1fdec13596ae73200984
Reza Alizadeh Majd <r.a.majd@gmail.com>
2020-10-20 11:55:37 +0330

    add bash function to mark a git repo as personal

---
+++ common/bash-personal-tweaks.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+make_git_personal() {
+    path="."
+    if [ "$#" -ne "0" ]; then
+        path="$1"
+    fi
+
+    git -C "$path" rev-parse
+    if [ "$?" -ne "0" ]; then
+        echo "Error: invalid git path"
+        exit 1
+    fi
+    git -C $path config commit.gpgsign off
+    git -C $path config user.name "Reza Alizadeh Majd"
+    git -C $path config user.email "r.a.majd@gmail.com"
+    echo "DONE"
+    exit 0
+}
