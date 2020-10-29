8285bb1148b519d572cfcb1c45a5cda362ae232d
Reza Alizadeh Majd <r.a.majd@gmail.com>
2020-10-29 14:15:30 +0330

    fix exit issue with make_git_persoanal function

--- common/bash-personal-tweaks.sh
+++ common/bash-personal-tweaks.sh
@@ -9,11 +9,11 @@ make_git_personal() {
     git -C "$path" rev-parse
     if [ "$?" -ne "0" ]; then
         echo "Error: invalid git path"
-        exit 1
+        return 1
     fi
     git -C $path config commit.gpgsign off
     git -C $path config user.name "Reza Alizadeh Majd"
     git -C $path config user.email "r.a.majd@gmail.com"
     echo "DONE"
-    exit 0
+    return 0
 }
