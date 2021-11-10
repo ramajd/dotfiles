0f8290d632c588ff22fe16124503bf6351dee0dc
Reza Alizadeh Majd <r.a.majd@gmail.com>
2021-11-10 14:40:59 +0330

    update desktop configuration

--- guix/config.scm
+++ guix/config.scm
@@ -1,111 +1,112 @@
-;; This is an operating system configuration template
+;; My operating system configuration for PantherX OS
+;; Time-stamp: <2021-11-10 14:39:21 panther>
 ;; for PantherX core Desktop
 
 (use-modules (gnu)
-             (gnu packages certs)
-             (gnu packages fonts)
-             (gnu packages kde-frameworks)
-             (gnu packages gnome)
-	           (guix packages)
-             (px packages accounts)
-             (px packages desktop)
+             (gnu packages admin)
+             (gnu packages emacs)
+             (gnu packages rsync)
+             (gnu packages screen)
+             (gnu packages ssh)
+             (gnu packages tmux)
+             (gnu packages version-control)
+             (gnu packages vim)
+             (gnu services desktop)
+             (gnu services docker)
+             (gnu services sddm)
+             (gnu services xorg)
              (px services desktop)
-             (px system)
-             (nongnu packages linux)
-             (nongnu system linux-initrd)
-             (srfi srfi-1))
+             (px services security-token)
+             (px system install))
 
-(use-package-modules screen ssh certs tls version-control rsync vim
-                     admin tmux)
+;;;
+;;; Service customizations
+;;;
 
-(use-service-modules docker sddm xorg)
+(define (modified-sddm-service-config config)
+  (sddm-configuration
+   (inherit config)
+   (xorg-configuration
+    (xorg-configuration
+     (extra-config `("Section \"InputClass\"\n"
+                     "   Identifier \"touchpad\"\n"
+                     "   Driver \"libinput\"\n"
+                     "   MatchIsTouchpad \"on\"\n"
+                     "   Option \"Tapping\" \"on\"\n"
+                     "EndSection\n"
+                     "\n"))))))
 
-(operating-system
+;;;
+;;; Operating system definition
+;;;
+
+(px-desktop-os
+ (operating-system
   (host-name "panther.local")
   (timezone "Asia/Tehran")
   (locale "en_US.utf8")
 
-  (kernel linux)
-  (initrd microcode-initrd)
-  (firmware (list linux-firmware))
-
   (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (target "/boot/efi")
-                (menu-entries (list
-                  (menu-entry
-                    (label "Arch Linux")
-                    (linux "/boot/vmlinuz-linux")
-                    (linux-arguments '(
-                      "load_video"
-                      "gfxpayload=keep" ; set
-                      "insmod gzio"
-                      "insmod part_gpt"
-                      "insmod fat"
-                      "root=/dev/sda3"))
-                    (initrd "/boot/initramfs-linux.img"))))))
+               (bootloader grub-efi-bootloader)
+               (targets '("/boot/efi"))
+               (menu-entries
+                (list
+                 (menu-entry
+                  (label "Manjaro Linux")
+                  (linux "/boot/vmlinuz-5.10-x86_64")
+                  (linux-arguments '("load_video" "gfxpayload=keep" ; set
+                                     "insmod gzio" "insmod part_gpt"
+                                     "insmod fat" "root=/dev/sda3"))
+                  (initrd "/boot/initramfs-5.10-x86_64.img"))))))
 
-  (file-systems (cons* (file-system
-                         (device (file-system-label "my-root"))
-                         (mount-point "/")
-                         (type "ext4"))
-                       (file-system
-                         (mount-point "/home")
-                         (device "/dev/sda4")
-                         (type "ext4"))
-                       (file-system
-                         (mount-point "/boot/efi")
-                         (device "/dev/sda1")
-                         (type "vfat"))
-                       %base-file-systems))
+  (file-systems
+   (cons* (file-system
+           (device (file-system-label "PX-ROOT"))
+           (mount-point "/")
+           (type "ext4"))
+          (file-system
+           (mount-point "/home")
+           (device "/dev/sda5")
+           (type "ext4"))
+          (file-system
+           (mount-point "/boot/efi")
+           (device "/dev/sda1")
+           (type "vfat"))
+          %base-file-systems))
 
   (swap-devices '("/dev/sda2"))
 
   (users (cons* (user-account
-                  (name "panther")
-                  (comment "PantherX default user")
-                  (group "users")
-                  (supplementary-groups '("wheel" "netdev"
-                                          "kvm" "docker"
-                                          "audio" "video")))
+                 (name "panther")
+                 (comment "PantherX default user")
+                 (group "users")
+                 (supplementary-groups '("wheel" "netdev"
+                                         "kvm" "docker"
+                                         "audio" "video")))
                 %base-user-accounts))
 
-  (skeletons (pantherx-skeletons))
-
   ;; Globally-installed packages.
-  (packages (cons* openssh nss-certs gnutls
-                   git vim rsync ;; development
-                   screen ;; terminal window manager
-                   tmux ;; personal packages
-                   wpa-supplicant
-                   %pantherx-packages))
+  (packages (cons*
+             ;; networking
+             openssh wpa-supplicant
+             ;; development
+             git vim emacs
+             ;; tools
+             rsync screen tmux
+             %px-desktop-packages))
 
-  ;; Add services to the baseline
-  (services (cons* (service sddm-service-type
-                            (sddm-configuration
-                              (minimum-uid 1000)
-                              (theme "darkine")
-                              (xorg-configuration
-                                (xorg-configuration
-                                  (extra-config `("Section \"InputClass\"\n"
-                                                  "   Identifier \"touchpad\"\n"
-                                                  "   Driver \"libinput\"\n"
-                                                  "   MatchIsTouchpad \"on\"\n"
-                                                  "   Option \"Tapping\" \"on\"\n"
-                                                  "EndSection\n"
-                                                  "\n"))))))
-                   (service docker-service-type)
-                   (service elogind-service-type
-                            (elogind-configuration
-                             (idle-action 'suspend)
-                             (idle-action-seconds (* 60 5))))
-		   (service sysctl-service-type
-			    (sysctl-configuration
-			      (settings '(("fs.inotify.max_user_watches" . "524288")))))
-                   (remove (lambda (svc)
-                             (or (eq? (service-kind svc) sddm-service-type)
-                                 (eq? (service-kind svc) elogind-service-type)))
-                           %pantherx-services)))
+  ;; System services
+  (services
+   (cons*
+    (service docker-service-type)
 
-  ;; Allow resolution of '.local' host names with mDNS.
-  (name-service-switch %mdns-host-lookup-nss))
+    (ledger-wallet-service)
+    (nitro-key-service)
+
+    (modify-services %pantherx-services
+                     (sddm-service-type config => (modified-sddm-service-config config))
+                     (elogind-service-type config => (elogind-configuration
+                                                      (inherit config)
+                                                      (idle-action 'suspend)
+                                                      (idle-action-seconds (* 60 5)))))))
+  ))
